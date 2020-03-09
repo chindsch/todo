d8b6536baa06b8d71ee4420f995d769e64ffab65
unknown <chindsch@stevens.edu>
2020-03-09 19:42:36 -0400

    Working with Forms

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 
-function App() {
+class TodoList extends Component {
+  constructor(){
+    super();
+    this.state = {
+      todos: [],
+      currentToDo: "",
+      isLoaded: false,
+    }
+  };
+
+//This method fires when input value changes. EVERY CHANGE IN INPUT
+  handleChange = event => {
+    this.setStste({
+      currentToDo:event.target.value,
+    })
+  };
+
+
+// This method fires when the form is submitted
+  addTask = event => {
+    event.preventDefault();
+    this.setState({
+      todos:[...this.state.todos,this.state.currentToDo]
+    })
+  };
+
+
+  delete = event => {
+    this.setState({ list: [] });
+    //addEventListener('click',removeEvent);
+  };
+
+  resetTasks = () => {
+    this.setState(this.state.todos)
+  }
+
+
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className ="container">
+    <form onSubmit={this.addTask}>
+      <label htmlFor = "taskName">Task Name: </label>
+      <input onChange={this.handleChange} name="taskName" type="text" placeholder="Add todo here"/>
+      <button type="button" value="Delete" onClick={this.delete}> </button>
+      <button type="submit"> Add Task </button>
+      </form>
     </div>
   );
-}
+};
+};
 
-export default App;
+export default TodoList;
